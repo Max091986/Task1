41a9bb6931442b3ba9bfa401f50b4b967a93bad6
max <max@max-HP-Pavilion-Notebook>
2023-11-27 17:24:54 +0500

    change task

--- Task/src/ua/com/foxminded/task/Task381.java
+++ Task/src/ua/com/foxminded/task/Task381.java
@@ -1,18 +1,45 @@
 package ua.com.foxminded.task;
 
+//Write a code that brings the number A into the integral-value deg N (N>0). 
+//A number and a deg are entered from a keyboard.
+//For instance: A = 2, N = 3, the answer is 8.
+
+import java.util.Scanner;
 public class Task381 {
 
 	public static void main(String[] args) {
-		int n = 1;
-//		
-		for(int i = n; i <= 360; i ++) {
-			if (i == 5)
-			break ;
-			
-				
-			
-				System.out.println(i);
-					
-	}
-	}
+		Scanner sc = new Scanner(System.in);
+	      int sum=0;
+	    	{
+	          System.out.println("Enter number A" + " ");
+	   }
+	      	 int a =  sc.nextInt();
+	         
+	    	 System.out.println("Enter Deg N from 0 to 360" + " ");
+	    
+	    	int n = sc.nextInt();
+	    	sum = a+n;
+	    	for(int i = sum; i<=360; i++) {
+	    		
+	    	}
+	    	
+	      if (sum <=30 ) {
+	    		System.out.println("N = 10");
+	    		}
+	   
+	   else if (sum>30 & sum<90) {
+	    		System.out.println("N = 20");	
+	    		}
+	   else if (sum>90 & sum<180) {
+	    		System.out.println("N = 30");
+	   }
+	   else	if (sum>180 & sum<360) {
+	    		System.out.println("N = 90");
+	   }
+	   else  {
+	    		 System.out.println("Sum = 0");
+	   }
+	     sc.close(); 
+	} 
 }
+
